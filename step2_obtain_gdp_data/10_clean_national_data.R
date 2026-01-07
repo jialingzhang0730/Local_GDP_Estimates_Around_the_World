ab1534b6854f1ec860b741266c0ff3f98e35e87f
jtsang020327 <jtsang327@gmail.com>
2026-01-07 17:27:14 -0600

    Updated to Version 2 (2012-2022)

--- step2_obtain_gdp_data/10_clean_national_data.R
+++ step2_obtain_gdp_data/10_clean_national_data.R
@@ -31,7 +31,7 @@ library(readxl)
 # we can first get most countries' GDP data from IMF
 
 # constant 2021 international dollars PPP adjusted
-imf_pop <- read_excel("version2_year2012_2022/step2_obtain_gdp_data/inputs/gdp_data/national/IMF_data/WEO_Data.xlsx")  %>% 
+imf_pop <- read_excel("step2_obtain_gdp_data/inputs/gdp_data/national/IMF_data/WEO_Data.xlsx")  %>% 
   filter(`Subject Descriptor` == "Population",
           Units == "Persons") %>%  # select population
   dplyr::select(-c("Subject Descriptor", "Units", "Scale", "Country/Series-specific Notes", "Estimates Start After"))  %>% 
@@ -42,7 +42,7 @@ imf_pop <- read_excel("version2_year2012_2022/step2_obtain_gdp_data/inputs/gdp_d
          population = as.numeric(value)*1e6)  %>% # the unit now is in how many persons
   dplyr::select(c(iso, year, population, Country))
 
-imf_gdp_const_2021_PPP <- read_excel("version2_year2012_2022/step2_obtain_gdp_data/inputs/gdp_data/national/IMF_data/WEO_Data.xlsx")  %>% 
+imf_gdp_const_2021_PPP <- read_excel("step2_obtain_gdp_data/inputs/gdp_data/national/IMF_data/WEO_Data.xlsx")  %>% 
   filter(`Subject Descriptor` == "Gross domestic product per capita, constant prices",
           Units == "Purchasing power parity; 2021 international dollar")  %>% # select current international dollars PPP adjusted
   dplyr::select(-c("Country", "Subject Descriptor", "Units", "Scale", "Country/Series-specific Notes", "Estimates Start After"))  %>% 
@@ -56,7 +56,7 @@ imf_gdp_const_2021_PPP <- read_excel("version2_year2012_2022/step2_obtain_gdp_da
   mutate(rgdp_total = rgdp_total * population/1e9) # now the unit is in billion constant 2021 international dollar
 
 # current international dollars PPP adjusted
-imf_gdp_crt_PPP <- read_excel("version2_year2012_2022/step2_obtain_gdp_data/inputs/gdp_data/national/IMF_data/WEO_Data.xlsx")  %>% 
+imf_gdp_crt_PPP <- read_excel("step2_obtain_gdp_data/inputs/gdp_data/national/IMF_data/WEO_Data.xlsx")  %>% 
   filter(`Subject Descriptor` == "Gross domestic product, current prices",
           Units == "Purchasing power parity; international dollars")  %>% # select current international dollars PPP adjusted
   dplyr::select(-c("Country", "Subject Descriptor", "Units", "Scale", "Country/Series-specific Notes", "Estimates Start After"))  %>% 
@@ -69,7 +69,7 @@ imf_gdp_crt_PPP <- read_excel("version2_year2012_2022/step2_obtain_gdp_data/inpu
   left_join(imf_pop)
 
 # current US dollars
-imf_gdp_crt_us <- read_excel("version2_year2012_2022/step2_obtain_gdp_data/inputs/gdp_data/national/IMF_data/WEO_Data.xlsx")  %>% 
+imf_gdp_crt_us <- read_excel("step2_obtain_gdp_data/inputs/gdp_data/national/IMF_data/WEO_Data.xlsx")  %>% 
   filter(`Subject Descriptor` == "Gross domestic product, current prices",
           Units == "U.S. dollars")  %>% # select current US dollars
   dplyr::select(-c("Country", "Subject Descriptor", "Units", "Scale", "Country/Series-specific Notes", "Estimates Start After"))  %>% 
@@ -85,7 +85,7 @@ imf_gdp_crt_us <- read_excel("version2_year2012_2022/step2_obtain_gdp_data/input
 # we need to change current US dollars to constant 2021 US dollars
 # The method to change current US dollars to constant 2021 US dollars is (according to United States Census Bureau website):
 #     value*(2021 index/current year index)
-index <- read_excel("version2_year2012_2022/step2_obtain_gdp_data/inputs/gdp_data/national/US_census_bureau_data/annual-index-value_annual-percent-change.xls", skip = 2)  %>% 
+index <- read_excel("step2_obtain_gdp_data/inputs/gdp_data/national/US_census_bureau_data/annual-index-value_annual-percent-change.xls", skip = 2)  %>% 
   rename(year = `Income year`, index = `C-CPI-U1\nIndex\n(December 1999 = 100)`)  %>% 
   dplyr::select(year, index)  %>% 
   na.omit() %>% # omit those "notes" that it reads
@@ -107,7 +107,7 @@ imf_gdp_const_2021 <- imf_gdp_crt_us  %>%
 iso_get_fm_wb <- c("BMU", "CYM", "CUW", "GRL", "XKX", "LIE", "MCO", "SXM", "SYR", "TCA", "PSE", "MNE")
 
 # population
-wb_pop <- read_excel("version2_year2012_2022/step2_obtain_gdp_data/inputs/gdp_data/national/world_bank_data/population/API_SP.POP.TOTL_DS2_en_excel_v2_19296.xls", sheet = "Data", skip = 3)  %>% 
+wb_pop <- read_excel("step2_obtain_gdp_data/inputs/gdp_data/national/world_bank_data/population/API_SP.POP.TOTL_DS2_en_excel_v2_19296.xls", sheet = "Data", skip = 3)  %>% 
   rename(iso = `Country Code`)  %>% 
   filter(iso %in% iso_get_fm_wb)  %>% # only select those isos that IMF misses and those we want from world bank 
   dplyr::select(-c("Indicator Name", "Indicator Code"))  %>% 
@@ -117,7 +117,7 @@ wb_pop <- read_excel("version2_year2012_2022/step2_obtain_gdp_data/inputs/gdp_da
   rename(population = value, Country = `Country Name`) # the unit is how many people
 
 # current US dollars
-wb_gdp_crt_us <- read_excel("version2_year2012_2022/step2_obtain_gdp_data/inputs/gdp_data/national/world_bank_data/GDP_current_USdollar/API_NY.GDP.MKTP.CD_DS2_en_excel_v2_19310.xls", sheet = "Data", skip = 3)  %>% 
+wb_gdp_crt_us <- read_excel("step2_obtain_gdp_data/inputs/gdp_data/national/world_bank_data/GDP_current_USdollar/API_NY.GDP.MKTP.CD_DS2_en_excel_v2_19310.xls", sheet = "Data", skip = 3)  %>% 
   rename(iso = `Country Code`)  %>% 
   filter(iso %in% iso_get_fm_wb)  %>% # only select those isos that IMF misses and those we want from world bank 
   dplyr::select(-c("Country Name", "Indicator Name", "Indicator Code"))  %>% 
@@ -137,7 +137,7 @@ wb_gdp_const_2021 <- wb_gdp_crt_us  %>%
 
 # current international dollars PPP adjusted
 # Note: "SYR", "LIE", "MCO" have missing data for this one
-wb_gdp_crt_PPP <- read_excel("version2_year2012_2022/step2_obtain_gdp_data/inputs/gdp_data/national/world_bank_data/GDP_PPP_current/API_NY.GDP.MKTP.PP.CD_DS2_en_excel_v2_19548.xls", sheet = "Data", skip = 3)  %>% 
+wb_gdp_crt_PPP <- read_excel("step2_obtain_gdp_data/inputs/gdp_data/national/world_bank_data/GDP_PPP_current/API_NY.GDP.MKTP.PP.CD_DS2_en_excel_v2_19548.xls", sheet = "Data", skip = 3)  %>% 
   rename(iso = `Country Code`)  %>% 
   filter(iso %in% iso_get_fm_wb)  %>% # only select those isos that IMF misses and those we want from world bank 
   dplyr::select(-c(`Country Name`, "Indicator Name", "Indicator Code"))  %>% 
@@ -150,7 +150,7 @@ wb_gdp_crt_PPP <- read_excel("version2_year2012_2022/step2_obtain_gdp_data/input
 
 # constant 2021 international dollars PPP adjusted:
 # we do not have LIE and MCO's data
-wb_gdp_const_2021_PPP <- read_excel("version2_year2012_2022/step2_obtain_gdp_data/inputs/gdp_data/national/world_bank_data/GDP_PPP_const_2021/API_NY.GDP.MKTP.PP.KD_DS2_en_excel_v2_20528.xls", sheet = "Data", skip = 3)  %>% 
+wb_gdp_const_2021_PPP <- read_excel("step2_obtain_gdp_data/inputs/gdp_data/national/world_bank_data/GDP_PPP_const_2021/API_NY.GDP.MKTP.PP.KD_DS2_en_excel_v2_20528.xls", sheet = "Data", skip = 3)  %>% 
   rename(iso = `Country Code`)  %>% 
   filter(iso %in% iso_get_fm_wb)  %>% # only select those isos that IMF misses and those we want from world bank 
   dplyr::select(-c(`Country Name`, "Indicator Name", "Indicator Code"))  %>% 
@@ -167,7 +167,7 @@ wb_gdp_const_2021_PPP <- read_excel("version2_year2012_2022/step2_obtain_gdp_dat
 #   Even though in the downloaded file, it said "Gross Domestic Product (GDP)", it is NOT.
 #   That means we need population data
 iso_get_fm_un <- c("CUB", "ERI", "PRK")
-pop_cub_eri_prk <- read_excel("version2_year2012_2022/step2_obtain_gdp_data/inputs/gdp_data/national/world_bank_data/population/API_SP.POP.TOTL_DS2_en_excel_v2_19296.xls", sheet = "Data", skip = 3)  %>% 
+pop_cub_eri_prk <- read_excel("step2_obtain_gdp_data/inputs/gdp_data/national/world_bank_data/population/API_SP.POP.TOTL_DS2_en_excel_v2_19296.xls", sheet = "Data", skip = 3)  %>% 
   rename(iso = `Country Code`, Country = `Country Name`)  %>% 
   filter(iso %in% iso_get_fm_un) %>% # only those we need 
   pivot_longer(cols = matches("\\d{4}"), names_prefix = "X", names_to = "year")  %>% 
@@ -177,7 +177,7 @@ pop_cub_eri_prk <- read_excel("version2_year2012_2022/step2_obtain_gdp_data/inpu
   filter(year >= 2012, year <= 2022) # We only need data from 2012 to 2022. The inclusion of 2022 is intentional to facilitate future updates.
 
 # GDP in current US dollars
-un_gdp_crt_us <- read.csv("version2_year2012_2022/step2_obtain_gdp_data/inputs/gdp_data/national/UN_data/UNdata_Export_20250426_211303453.csv") %>% 
+un_gdp_crt_us <- read.csv("step2_obtain_gdp_data/inputs/gdp_data/national/UN_data/UNdata_Export_20250426_211303453.csv") %>% 
   mutate(iso = ifelse(`Country.or.Area` == "Cuba", "CUB",
                ifelse(`Country.or.Area` == "Democratic People's Republic of Korea", "PRK",
                ifelse(`Country.or.Area` == "Eritrea", "ERI", NA))))  %>% 
@@ -205,10 +205,10 @@ national_gdp_const_2021_USD <- bind_rows(imf_gdp_const_2021, wb_gdp_const_2021, 
 national_gdp_current_PPP <- bind_rows(imf_gdp_crt_PPP, wb_gdp_crt_PPP)
 national_gdp_const_2021_PPP <- bind_rows(imf_gdp_const_2021_PPP, wb_gdp_const_2021_PPP)
 
-write.csv(national_gdp_current_USD, "version2_year2012_2022/step2_obtain_gdp_data/temp/national_gdp_current_USD.csv", row.names = F)
-write.csv(national_gdp_const_2021_USD, "version2_year2012_2022/step2_obtain_gdp_data/temp/national_gdp_const_2021_USD.csv", row.names = F)
-write.csv(national_gdp_current_PPP, "version2_year2012_2022/step2_obtain_gdp_data/temp/national_gdp_current_PPP.csv", row.names = F)
-write.csv(national_gdp_const_2021_PPP, "version2_year2012_2022/step2_obtain_gdp_data/temp/national_gdp_const_2021_PPP.csv", row.names = F)
+write.csv(national_gdp_current_USD, "step2_obtain_gdp_data/temp/national_gdp_current_USD.csv", row.names = F)
+write.csv(national_gdp_const_2021_USD, "step2_obtain_gdp_data/temp/national_gdp_const_2021_USD.csv", row.names = F)
+write.csv(national_gdp_current_PPP, "step2_obtain_gdp_data/temp/national_gdp_current_PPP.csv", row.names = F)
+write.csv(national_gdp_const_2021_PPP, "step2_obtain_gdp_data/temp/national_gdp_const_2021_PPP.csv", row.names = F)
 
 # ------------------------------------------------- #
 # calculate the GDP per capita
@@ -222,9 +222,9 @@ national_gdpc_current_PPP <- national_gdp_current_PPP %>%
 national_gdpc_const_2021_PPP <- national_gdp_const_2021_PPP %>% 
   mutate(national_gdpc = rgdp_total/population)
 
-write.csv(national_gdpc_current_USD, "version2_year2012_2022/step2_obtain_gdp_data/temp/national_gdpc_current_USD.csv", row.names = F)
-write.csv(national_gdpc_const_2021_USD, "version2_year2012_2022/step2_obtain_gdp_data/temp/national_gdpc_const_2021_USD.csv", row.names = F)
-write.csv(national_gdpc_current_PPP, "version2_year2012_2022/step2_obtain_gdp_data/temp/national_gdpc_current_PPP.csv", row.names = F)
-write.csv(national_gdpc_const_2021_PPP, "version2_year2012_2022/step2_obtain_gdp_data/temp/national_gdpc_const_2021_PPP.csv", row.names = F)
+write.csv(national_gdpc_current_USD, "step2_obtain_gdp_data/temp/national_gdpc_current_USD.csv", row.names = F)
+write.csv(national_gdpc_const_2021_USD, "step2_obtain_gdp_data/temp/national_gdpc_const_2021_USD.csv", row.names = F)
+write.csv(national_gdpc_current_PPP, "step2_obtain_gdp_data/temp/national_gdpc_current_PPP.csv", row.names = F)
+write.csv(national_gdpc_const_2021_PPP, "step2_obtain_gdp_data/temp/national_gdpc_const_2021_PPP.csv", row.names = F)
 
 
