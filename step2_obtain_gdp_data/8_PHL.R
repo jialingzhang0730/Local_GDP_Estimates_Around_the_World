f157f4e5e03ca236d490d1eaf430adca63a13a77
jtsang020327 <jtsang327@gmail.com>
2026-01-07 17:35:10 -0600

    Updated to Version 2 (2012-2022)

--- step2_obtain_gdp_data/8_PHL.R
+++ step2_obtain_gdp_data/8_PHL.R
@@ -15,7 +15,7 @@ library(sf)
 # ------------------------------------------------- #
 # Obtain GDP data: 
 
-PHL_regional_rgdp <- read_excel("version2_year2012_2022/step2_obtain_gdp_data/inputs/gdp_data/regional/PHL/GRDP_Reg_2018PSNA_2000-2023.xlsx", skip = 9, n_max = 18)  %>% 
+PHL_regional_rgdp <- read_excel("step2_obtain_gdp_data/inputs/gdp_data/regional/PHL/GRDP_Reg_2018PSNA_2000-2023.xlsx", skip = 9, n_max = 18)  %>% 
        slice(-1)  %>% # remove the first row, they are missing values
        dplyr::select(-1) %>% # remove the first column, we do not need that
        rename(admin_2_name = ...2)  %>% 
@@ -31,7 +31,7 @@ PHL_regional_rgdp <- read_excel("version2_year2012_2022/step2_obtain_gdp_data/in
               min_admin_unit = 2, admin_1_name = "Philippines") %>% 
        dplyr::select(id, iso, year, min_admin_unit, starts_with("admin_2"), starts_with("admin_1"))
 
-write.csv(PHL_regional_rgdp, "version2_year2012_2022/step2_obtain_gdp_data/temp/phl_gdp_clean.csv", row.names = F)
+write.csv(PHL_regional_rgdp, "step2_obtain_gdp_data/temp/phl_gdp_clean.csv", row.names = F)
 
 # ------------------------------------------------- #
 # Create training data
@@ -43,12 +43,12 @@ training_df <- PHL_regional_rgdp %>%
   dplyr::select(id, year, iso, unit_name, min_admin_unit, matches("unit_rgdp"),
          parent_admin_unit, parent_name, matches("parent_rgdp"))
 
-write.csv(training_df, "version2_year2012_2022/step2_obtain_gdp_data/temp/phl_training_data.csv", row.names = F)
+write.csv(training_df, "step2_obtain_gdp_data/temp/phl_training_data.csv", row.names = F)
 
 # ------------------------------------------------- #
 # Create shapefiles
 
-PHL_regional_sf <- read_sf("version2_year2012_2022/step1_obtain_gis_data/outputs/CGAZ_ADM1_without_large_waters.gpkg") %>%  
+PHL_regional_sf <- read_sf("step1_obtain_gis_data/outputs/CGAZ_ADM1_without_large_waters.gpkg") %>%  
   rename(name = shapeName, iso = shapeGroup) %>%
   filter(iso == "PHL") %>%
   mutate(name = case_when(name == "ARMM" ~ "Bangsamoro Autonomous Region\r\nin Muslim Mindanao",
@@ -68,7 +68,7 @@ PHL_national_sf <- PHL_regional_sf %>%
   summarize(geom = st_union(geom),
             admin_unit = 1, .groups = "drop")
 
-st_write(PHL_regional_sf, "version2_year2012_2022/step2_obtain_gdp_data/temp/phl_admin_2.gpkg", append = F)
-st_write(PHL_national_sf, "version2_year2012_2022/step2_obtain_gdp_data/temp/phl_admin_1.gpkg", append = F)
+st_write(PHL_regional_sf, "step2_obtain_gdp_data/temp/phl_admin_2.gpkg", append = F)
+st_write(PHL_national_sf, "step2_obtain_gdp_data/temp/phl_admin_1.gpkg", append = F)
 
 # eof ----
